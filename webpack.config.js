31d6c1efc49020338c55e72cdc0d389dc67ec915
Nataliia Byshovets <natashabysh11@gmail.com>
2021-03-21 13:58:40 +0200

    webplack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "/goit-js-hw-08-gallery/",
   },
   module: {
     rules: [
